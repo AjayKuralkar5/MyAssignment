35bc4e630bc05e28ed28ab1ee846dfc697155c43
AjayKuralkar5 <ajayvkuralkar@gmail.com>
2026-01-03 20:51:43 +0530

    Add files via upload
    Day1

---
+++ Assignment/Chrome.java
@@ -0,0 +1,24 @@
+package week1.day1.Assignment;
+
+public class Chrome {
+	
+	public static void main(String[]args) {
+		
+		float percentage =91.0f;
+		System.out.println(percentage);
+		
+		String str = "Google";
+		System.out.println(str);
+		
+		boolean beta= false;
+		System.out.println(beta);
+		
+		int year=2008;
+		System.out.println(year);
+		
+		char shortcutKey='C';
+		System.out.println(shortcutKey);
+		
+	}
+
+}
