b7836747050bd52a3a1fe9c03b0d92b6e9c7dba5
AjayKuralkar5 <ajayvkuralkar@gmail.com>
2026-02-05 13:26:48 +0530

    Add files via upload
    Week5 Assignment

---
+++ week5/day1/CreateLeads.java
@@ -0,0 +1,19 @@
+package week5.day1;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+public class CreateLeads extends ProjectSpecificMethod {
+	
+	@Test
+	public void createLeads() {
+	driver.findElement(By.linkText("CRM/SFA")).click();
+	driver.findElement(By.linkText("Leads")).click();
+	driver.findElement(By.linkText("Create Lead")).click();
+	driver.findElement(By.id("createLeadForm_companyName")).sendKeys("TestLeaf");
+	driver.findElement(By.id("createLeadForm_firstName")).sendKeys("Hari");
+	driver.findElement(By.id("createLeadForm_lastName")).sendKeys("R");
+	driver.findElement(By.name("submitButton")).click();
+
+}
+}
